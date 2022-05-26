195bc2591a6064eb512397d9733526095286fd35
Thicules <caothiu2003@gmail.com>
2022-05-27 00:04:39 +0700

    add Nhan Vat

--- VoLamTruyenKi/soure/VoLamTruyenKI/VoLamTruyenKI/NhanVat.cpp
+++ VoLamTruyenKi/soure/VoLamTruyenKI/VoLamTruyenKI/NhanVat.cpp
@@ -25,25 +25,70 @@ void NhanVat::coutThongTin()
 //tiểu long nữ
 TieuLongNu::TieuLongNu() {
 	TenNhanVat = "Tieu Long Nu";
+	MonPhai = "Thien vuong bang";
+	He = "Kim";
 }
 // //Dương Quá
-// //Quách Tĩnh
+DuongQua::DuongQua() {
+	TenNhanVat = "Duong Qua";
+	MonPhai = "Thieu Lam";
+	He = "Kim";
+}
+//Quách Tĩnh
+QuachTinh::QuachTinh() {
+	TenNhanVat = "Quach Tinh";
+	MonPhai = "Ngu Doc Giao";
+	He = "Moc";
+}
 //Âu Dương Phong
+AuDuongPhong::AuDuongPhong() {
+	TenNhanVat = "Au Duong Phong";
+	MonPhai = "Duong Mon";
+	He = "Moc";
+}
 // Doãn Chí Bình
+DoanChiBinh::DoanChiBinh() {
+	TenNhanVat = "Doan Chi Binh";
+	MonPhai = "Nga My";
+	He = "Thuy";
+}
 // Kiều Phong
+KieuPhong::KieuPhong() {
+	TenNhanVat = "Kieu Phong";
+	MonPhai = "Thuy Yen Mon";
+	He = "Thuy";
+}
 // Hư Túc
+HuTuc::HuTuc() {
+	TenNhanVat = "Hu Tuc";
+	MonPhai = "Cai Bang";
+	He = "Hoa";
+}
 // Đoàn Dự
+DoanDu::DoanDu() {
+	TenNhanVat = "Doan Du";
+	MonPhai = "Thien Nhan Giao";
+	He = "Hoa";
+}
 // Trương Vô Kị
+TruongVoKi::TruongVoKi(){
+	TenNhanVat = "Truong Vo ki";
+	MonPhai = "Con Lon";
+	He = "Tho";
+}
 // Lý Mạc Sầu
-// Vi Tiểu Bảo
-// Độc Cô Cầu Bại
+LyMacSau::LyMacSau() {
+	TenNhanVat = "Ly Mac Sau";
+	MonPhai = "Vo Dang";
+	He = "Tho";
+}
 //  Chưa viết 
 
 string kiemtraHe(string MonPhai)
 {
 	if (MonPhai == "Thieu Lam")
 		return "Kim";
-	if (MonPhai == "Vuong Bang")
+	if (MonPhai == "Thien Vuong Bang")
 		return "Kim";
 
 	if (MonPhai == "Ngu Doc Giao")
@@ -62,9 +107,9 @@ string kiemtraHe(string MonPhai)
 		return "Hoa";
 
 	if (MonPhai == "Con Lon")
-		return "Hoa";
+		return "Tho";
 	if (MonPhai == "Vo Dang")
-		return "Hoa";
+		return "Tho";
 }
 
 float TiledameA(string HeA, string HeB)
